78c829687f108038f60648e23c5601aa6b81c605
AliAais <aaisali228@gmail.com>
2024-04-09 07:13:02 -0700

    adding countires suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import contries from "./constants/contries";
 import "./App.css";
 
 function App() {
@@ -6,8 +7,11 @@ function App() {
   const [weatherData, setWeatherData] = useState({});
   const [loading, setLoading] = useState(false);
   const [invalid, setInvalid] = useState("");
+  const [showCountries, setShowCountries] = useState(false);
+  const [countries, setCountries] = useState(contries);
 
   const searchWeather = async () => {
+    setCity('');
     try {
       setLoading(true);
       setInvalid("");
@@ -31,61 +35,126 @@ function App() {
     }
   };
 
-  function getLocation() {
+  function getLocationAndSetWeather() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position);
+        setLoading(true);
+        fetch(
+          `https://api.weatherapi.com/v1/current.json?key=${
+            import.meta.env.VITE_API_KEY
+          }&q=${position.coords.latitude},${position.coords.longitude}&aqi=no`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setWeatherData(data);
+            setLoading(false);
+          });
       });
     } else {
+      setLoading(false);
       alert("Geolocation is not supported by this browser.");
     }
   }
 
   useEffect(() => {
-    searchWeather();
+    getLocationAndSetWeather();
   }, []);
 
-  getLocation();
+  useEffect(()=>{
+ const filterArr=contries.filter((country,index)=>{
+  if(country.value.toLowerCase().includes(city.toLowerCase())){
+    return country;
+  }
+ })
+ console.log(filterArr);
+ setCountries(filterArr);
+ },[city])
+
   return (
     <>
-    <div className="container">
-      <div className="ipContainer">
-        <input
-          type="text"
-          className="ip"
-          placeholder="Enter city"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button className="bt" onClick={searchWeather}>
-          Search
-        </button>
-      </div>
-      <p style={{ color: "red", fontWeight: "bold", textAlign: "center" }}>
-          {invalid}
-        </p>
-      <div>
-        {loading ? (
-          <p style={{ textAlign: "center" }}>Loading...</p>
-        ) : (
-          invalid.length == 0 && (
+      <div className="container">
+        <div className="ipContainer" style={{ position: "relative" }}>
+          <input
+            type="text"
+            className="ip"
+            placeholder="Enter city"
+            value={city}
+            onClick={()=>setShowCountries(true)}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button className="bt" onClick={searchWeather}>
+            Search
+          </button>
+          {showCountries> 0 && (
             <div
               style={{
-                textAlign: "center",
-                border: "1px solid white",
-                marginTop: "15px",
+                backgroundColor: "white",
+                position: "absolute",
+                width: "97.8%",
+                padding: "5px",
+                display: "flex",
+                flexDirection: "column",
+                borderCollapse: "collapse",
+                color: "black",
+                height: "120px",
+                overflowY: "scroll",
+                bottom: -125,
+                right:0
               }}
             >
-              <p>City Name : {weatherData?.location?.name}</p>
-              <p>Region : {weatherData?.location?.region}</p>
-              <p>Temperature : {weatherData?.current?.temp_c} C</p>
-              <p>Weather : {weatherData?.current?.condition.text}</p>
-              <p>Wind : {weatherData?.current?.wind_kph} kph</p>
+              {countries.map((country, index) => {
+                return (
+                  <p
+                    key={index}
+                    style={{
+                      margin: 0,
+                      padding: "5px 0px",
+                      borderBottom: "1px solid gray",
+                      cursor: "pointer",
+                    }}
+                    onMouseEnter={(e) => {
+                      e.target.style.backgroundColor = "gray";
+                    }}
+                    onMouseOut={(e) => {
+                      e.target.style.backgroundColor = "white";
+                    }}
+                    onClick={() => {
+                      setShowCountries(false)
+                      setCity(country.value)
+                    }}
+                  >
+                    {country.value}
+                  </p>
+                );
+              })}
             </div>
-          )
-        )}
+          )}
+        </div>
+        <p style={{ color: "red", fontWeight: "bold", textAlign: "center" }}>
+          {invalid}
+        </p>
+        <div>
+          {loading ? (
+            <p style={{ textAlign: "center" }}>Loading...</p>
+          ) : (
+            invalid.length == 0 && (
+              <div
+                style={{
+                  textAlign: "center",
+                  border: "1px solid white",
+                  marginTop: "15px",
+                }}
+              >
+                <p>City Name : {weatherData?.location?.name}</p>
+                <p>Region : {weatherData?.location?.region}</p>
+                <p>Temperature : {weatherData?.current?.temp_c} C</p>
+                <p>Weather : {weatherData?.current?.condition.text}</p>
+                <p>Wind : {weatherData?.current?.wind_kph} kph</p>
+              </div>
+            )
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div>Get Weather</div>
+        <p>About</p>
+    </div>
+  )
+}
+
+export default Header
